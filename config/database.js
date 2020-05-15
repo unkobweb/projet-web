26b5aa82301087de57c5fe47a17ceeba27332a32
Kobweb <yugoplixgamer@gmail.com>
2020-05-15 16:44:25 +0200

    Connect the ORM (Sequelize) to the postgre DB

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+module.exports = new Sequelize(
+  "kobweb_projet_web",
+  "kobweb_node",
+  "simonandalex44200",
+  {
+    host: "postgresql-kobweb.alwaysdata.net",
+    dialect: "postgres",
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
