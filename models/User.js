c4dacbff5f432b4381f1bb65d61a2264d8e56909
Kobweb <yugoplixgamer@gmail.com>
2020-05-15 16:44:36 +0200

    Add model of user

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database");
+
+const User = db.define(
+  "user",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    username: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    date_of_birth: {
+      type: Sequelize.DATEONLY,
+    },
+    balance: {
+      type: Sequelize.FLOAT,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+module.exports = User;
