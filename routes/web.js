c711a237e3376bd71d58750672f6e97923273557
Kobweb <yugoplixgamer@gmail.com>
2020-05-18 15:51:18 +0200

    Add route /carts to get all carts

--- routes/web.js
+++ routes/web.js
@@ -4,6 +4,7 @@ const db = require("../config/database");
 const User = require("../models/User");
 const Game = require("../models/Game");
 const CdKey = require("../models/CdKey");
+const Cart = require("../models/Cart");
 
 router.get("/users", (req, res) => {
   User.findAll()
@@ -18,6 +19,11 @@ router.get("/games", (req, res) => {
 router.get("/cdkeys", (req, res) => {
   CdKey.findAll()
     .then((cdkeys) => res.send(cdkeys))
+    .catch((err) => console.log(err));
+});
+router.get("/carts", (req, res) => {
+  Cart.findAll()
+    .then((carts) => res.send(carts))
     .catch((err) => console.log(err));
 });
 
