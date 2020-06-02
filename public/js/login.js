80a2ddccb6c7ccaf4cea462965b3d880dc088d68
Kobweb <yugoplixgamer@gmail.com>
2020-06-02 14:58:22 +0200

    Add google / facebook connexion

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,18 @@
 let login = new Vue({
   el: "#login",
   data: {
+    firebaseConfig: {
+      apiKey: "AIzaSyBNHbpw_xyCeYmm30OtArbZe7SFCteL34E",
+      authDomain: "to-game-together.firebaseapp.com",
+      databaseURL: "https://to-game-together.firebaseio.com",
+      projectId: "to-game-together",
+      storageBucket: "to-game-together.appspot.com",
+      messagingSenderId: "452035355461",
+      appId: "1:452035355461:web:80649ad19e39cee86d142c",
+      measurementId: "G-LHT975FTVS",
+    },
+    googleProvider: null,
+    facebookProvider: null,
     message: "",
     mail: "",
     password: "",
@@ -26,5 +38,38 @@ let login = new Vue({
           }
         });
     },
+    externalLogin: function (provider) {
+      firebase
+        .auth()
+        .signInWithPopup(eval(provider))
+        .then(function (result) {
+          console.log(result.user);
+          fetch("/externalConnexion", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: result.user.uid,
+              name: result.user.displayName,
+              mail: result.user.email,
+            }),
+          }).then(() => {
+            window.location.href = "/";
+          });
+        })
+        .catch(function (error) {
+          console.log(error.code);
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          // ...
+        });
+    },
+  },
+  created: function () {
+    firebase.initializeApp(this.firebaseConfig);
+    firebase.analytics();
+
+    this.googleProvider = new firebase.auth.GoogleAuthProvider();
+    this.facebookProvider = new firebase.auth.FacebookAuthProvider();
   },
 });
--- public/js/register.js
+++ public/js/register.js
@@ -6,6 +6,18 @@ function validateEmail(email) {
 let register = new Vue({
   el: "#register",
   data: {
+    firebaseConfig: {
+      apiKey: "AIzaSyBNHbpw_xyCeYmm30OtArbZe7SFCteL34E",
+      authDomain: "to-game-together.firebaseapp.com",
+      databaseURL: "https://to-game-together.firebaseio.com",
+      projectId: "to-game-together",
+      storageBucket: "to-game-together.appspot.com",
+      messagingSenderId: "452035355461",
+      appId: "1:452035355461:web:80649ad19e39cee86d142c",
+      measurementId: "G-LHT975FTVS",
+    },
+    googleProvider: null,
+    facebookProvider: null,
     message: "",
     username: "",
     mail: "",
@@ -53,5 +65,38 @@ let register = new Vue({
           });
       }
     },
+    externalLogin: function (provider) {
+      firebase
+        .auth()
+        .signInWithPopup(eval(provider))
+        .then(function (result) {
+          console.log(result.user);
+          fetch("/externalConnexion", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: result.user.uid,
+              name: result.user.displayName,
+              mail: result.user.email,
+            }),
+          }).then(() => {
+            window.location.href = "/";
+          });
+        })
+        .catch(function (error) {
+          console.log(error.code);
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          // ...
+        });
+    },
+  },
+  created: function () {
+    firebase.initializeApp(this.firebaseConfig);
+    firebase.analytics();
+
+    this.googleProvider = new firebase.auth.GoogleAuthProvider();
+    this.facebookProvider = new firebase.auth.FacebookAuthProvider();
   },
 });
