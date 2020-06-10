8d821548d83e7a5560c10b5852509eb6853a2d0b
Kobweb <yugoplixgamer@gmail.com>
2020-06-10 23:59:53 +0200

    Add the game page

--- routes/web.js
+++ routes/web.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const homeController = require("../controllers/homeController");
+const gameController = require("../controllers/gameController");
 const authController = require("../controllers/authController");
 
-router.get("/", homeController.index);
+router.get("/", gameController.index);
 router.get("/register", authController.indexRegister);
 router.post("/register", urlencodedParser, authController.register);
 router.get("/login", authController.indexLogin);
 router.post("/login", authController.login);
 router.post("/externalConnexion", authController.externalConnexion);
+router.get("/game/:id", gameController.show);
 
 module.exports = router;
