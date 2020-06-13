54570fc02ed7c35c9ef6da692298c08da7e51649
Kobweb <yugoplixgamer@gmail.com>
2020-06-13 16:30:44 +0200

    Update app.js
    Add sequelize create function in comment

--- app.js
+++ app.js
@@ -27,3 +27,54 @@ app.use("/", require("./routes/web"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
+
+/*
+Game.create({
+  title: "Cyberpunk 2077",
+  description:
+    "Cyberpunk 2077 pour PC est un jeu de tir à la première personne, mais avec une différence. Dans un État libre dystopique de Californie, les règles de la nation et de l'Etat ne s'appliquent plus. Au lieu de cela, jouant comme un mercenaire nommé V, le joueur doit travailler sur le chemin qu'il emprunte autour de la ville, atteignant ses buts et se battant contre des ennemis qui apparaissent au fur et à mesure du jeu.",
+  slug: "cyberpunk_2077",
+  quantity: 20,
+  price: 69.99,
+  discount: 20,
+  plateform_id: 2,
+});
+
+User.create({
+  id: "abc123",
+  username: "kobweb",
+  email: "pom@pom.com",
+}).then((user) => {
+  Cart.create({
+  userId: "abc123",
+  gameId: 1,
+  quantity: 2,
+});
+});
+
+Game.create({
+  title: "Cyberpunk 2077",
+  description:
+    "Cyberpunk 2077 pour PC est un jeu de tir à la première personne, mais avec une différence. Dans un État libre dystopique de Californie, les règles de la nation et de l'Etat ne s'appliquent plus. Au lieu de cela, jouant comme un mercenaire nommé V, le joueur doit travailler sur le chemin qu'il emprunte autour de la ville, atteignant ses buts et se battant contre des ennemis qui apparaissent au fur et à mesure du jeu.",
+  slug: "cyberpunk_2077",
+  quantity: 20,
+  price: 69.99,
+  discount: 20,
+  plateform_id: 4,
+});
+Game.create({
+  title: "Cyberpunk 2077",
+  description:
+    "Cyberpunk 2077 pour PC est un jeu de tir à la première personne, mais avec une différence. Dans un État libre dystopique de Californie, les règles de la nation et de l'Etat ne s'appliquent plus. Au lieu de cela, jouant comme un mercenaire nommé V, le joueur doit travailler sur le chemin qu'il emprunte autour de la ville, atteignant ses buts et se battant contre des ennemis qui apparaissent au fur et à mesure du jeu.",
+  slug: "cyberpunk_2077",
+  quantity: 20,
+  price: 69.99,
+  discount: 20,
+  plateform_id: 1,
+});
+Plateform.create({
+  name: "Steam",
+  slug: "steam",
+});
+Plateform.create({
+  name: "Playstation 4",
