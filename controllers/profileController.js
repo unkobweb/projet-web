b6b967a1ec95b2ec02722df072c032d10d33785f
Kobweb <sieg.alexandre@gmail.com>
2020-06-17 01:06:01 +0200

    Add succeed function

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,4 +1,11 @@
 const User = require("../models/").User;
+const Cart = require("../models/").Cart;
+const Game = require("../models/").Game;
+
+const stripe = require("stripe")(
+  "sk_test_51GukP3KkoFC8y2MeU06o6UMEHXUGNVOtlig0hsIEaodq7S75Uv7D9OF1Ghs6QHjXHOiqorh34qMpwRZwlFn3OBEs00Vxtzhy8A",
+  { apiVersion: "" }
+);
 
 function index(req, res) {
   res.render("profile.ejs", {
@@ -33,6 +40,19 @@ async function checkout(req, res) {
   } else {
     res.redirect("/cart");
   }
+}
+
+async function succeed(req, res) {
+  let oldCarts = await Cart.findAll({ where: { userId: req.session.user.id } });
+  oldCarts.forEach((old) => {
+    old.destroy();
+  });
+  let newUser = await User.findOne({
+    where: { id: req.session.user.id },
+    include: [{ model: Cart, include: [Game] }],
+  });
+  req.session.user = newUser;
+  res.sendStatus(200);
 }
 
 module.exports = { index, checkout, succeed };
