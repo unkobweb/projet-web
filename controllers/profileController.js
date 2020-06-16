6f696ecbef02ce553cb0f3d883bef00e0f2f1258
Kobweb <sieg.alexandre@gmail.com>
2020-06-17 01:05:41 +0200

    Add checkout function for payment

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -7,4 +7,32 @@ function index(req, res) {
   });
 }
 
-module.exports = { index };
+async function checkout(req, res) {
+  if (req.session.user != undefined && req.session.user.Carts.length > 0) {
+    let total = 0;
+    req.session.user.Carts.forEach((jeu) => {
+      total += parseFloat(
+        (jeu.Game.price - jeu.Game.price * (jeu.Game.discount / 100)).toFixed(
+          2
+        ) * jeu.quantity
+      );
+    });
+    console.log(total);
+    total = total.toString().split(".").join("");
+    console.log(total);
+    let paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "eur",
+      metadata: { integration_check: "accept_a_payment" },
+    });
+    res.render("checkout.ejs", {
+      session: req.session.user,
+      nbPage: 0,
+      paymentIntent: paymentIntent,
+    });
+  } else {
+    res.redirect("/cart");
+  }
+}
+
+module.exports = { index, checkout, succeed };
